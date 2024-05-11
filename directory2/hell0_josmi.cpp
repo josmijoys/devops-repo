92f196e74d7a6dffa37b22ad13513fc31855a0fd
josmi <josmijoys393@gmail.com>
2024-05-11 20:23:23 +0530

    first commit

---
+++ directory2/hell0_josmi.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(){
+	std::cout<<"Hello JOSMI....."<<std::endl;
+	std::cout<<"new line added......."<<std::endl;
+	return 0;
+}
---
+++ directory2/hello.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+
+int main(){
+	std::cout<<"Hello"<<std::endl;
+	std::cout<<"adding from newBranch..."<<std::endl;
+	return 0;
+}
