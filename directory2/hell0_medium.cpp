9510100f407b2ccbb2adbb2174aeed8e4f4fba61
Josmi Joys <74772443+josmijoys@users.noreply.github.com>
2024-08-24 11:11:34 +0530

    Update and rename hell0_josmi.cpp to hell0_medium.cpp

--- directory2/hell0_medium.cpp
+++ directory2/hell0_medium.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main(){
-	std::cout<<"Hello JOSMI....."<<std::endl;
+	std::cout<<"Hello MEDIUM....."<<std::endl;
 	std::cout<<"new line added......."<<std::endl;
 	return 0;
 }
